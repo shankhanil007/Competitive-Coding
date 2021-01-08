e11f7ea330a470d998404e28e9ddc5e969135699
shankhanil007 <shankhanilborthakur@gmail.com>
2021-01-08 21:32:44 +0530

    Added New File

---
+++ CP/Codeforces/Rounds/659 Div 2/A_Wizard_of_Orz.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define sz(x) (int)x.size()
+#define all(x) x.begin(), x.end()
+#define deb(x) cout << #x << " : " << x << "\n";
+#define debug(x, y) cout << #x << " : " << x << "\t" << #y << " : " << y << "\n";
+#define mod 1000000007
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, n) for (i = 1; i <= n; i++)
+#define fok(i, k, n) for (i = k; i <= n; i++)
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int q;
+    cin >> q;
+    for (int w = 1; w <= q; w++)
+    {
+        int n;
+        cin >> n;
+        if (n == 1)
+            cout << "9" << endl;
+        else if (n == 2)
+            cout << "98" << endl;
+        else if (n == 3)
+            cout << "989" << endl;
+        else
+        {
+            cout << "989";
+            int x = 0;
+            for (int i = 4; i <= n; i++)
+            {
+                cout << x;
+                x++;
+                if (x > 9)
+                {
+                    x = 0;
+                }
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+}
